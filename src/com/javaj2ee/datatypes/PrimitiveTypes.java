ed00a979c38b7b02d0a9eacc8231b24ba69b0dbf
saikat <saikatgupta20@gmail.com>
2026-02-07 02:10:38 +0530

    primitive types

--- src/com/javaj2ee/datatypes/PrimitiveTypes.java
+++ src/com/javaj2ee/datatypes/PrimitiveTypes.java
@@ -1,7 +1,15 @@
 package com.javaj2ee.datatypes;
 
 public class PrimitiveTypes {
-    public  static void main(String[] args){
+    public static void main(String[] args){
+        int i =16;
+        boolean flag = true;
+        double d = 12;
+        float f =13;
+        char c = 'a';
+        long l =11;
+        byte b =127;
+        System.out.println("print the byte"+b);
 
     }
 }
